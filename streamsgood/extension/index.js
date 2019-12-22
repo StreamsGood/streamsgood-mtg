2b33c0e49b066c28ec93d87d40f149300bc04c4e
Brendan Hagan <bhaganksu@gmail.com>
2019-12-22 16:18:57 -0500

    misc: updates to try and handle multi-client replicant mutations

--- streamsgood/extension/index.js
+++ streamsgood/extension/index.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const replicantDefaults = {
 	name: '',
 	deck: '',
@@ -6,6 +8,12 @@ const replicantDefaults = {
 	poison: 0,
 	games: 0
 };
+
+function generateMutationId() {
+  var array = new Uint32Array(1);
+  crypto.randomFillSync(array);
+  return array.toString();
+}
 
 module.exports = nodecg => {
 	const game = {
@@ -17,7 +25,10 @@ module.exports = nodecg => {
 		Object.keys(replicantDefaults).forEach(prop => {
 			game[player][prop] = nodecg.Replicant(`${player}-${prop}`, {
 				persistent: true,
-				defaultValue: replicantDefaults[prop]
+				defaultValue: {
+          mutationId: generateMutationId(),
+          innerValue: replicantDefaults[prop]
+        }
 			});
 		});
 	});
@@ -25,7 +36,10 @@ module.exports = nodecg => {
 	nodecg.listenFor('resetLifeTotals', () => {
 		Object.keys(game).forEach(player => {
 			['life', 'poison'].forEach(prop => {
-				game[player][prop].value = replicantDefaults[prop];
+				game[player][prop].value = {
+          mutationId: generateMutationId(),
+          innerValue: replicantDefaults[prop]
+        }
 			});
 		});
 	});
@@ -33,7 +47,10 @@ module.exports = nodecg => {
 	nodecg.listenFor('resetAll', () => {
 		Object.keys(game).forEach(player => {
 			Object.keys(replicantDefaults).forEach(prop => {
-				game[player][prop].value = replicantDefaults[prop];
+				game[player][prop].value = {
+          mutationId: generateMutationId(),
+          innerValue: replicantDefaults[prop]
+        }
 			});
 		});
 	});
